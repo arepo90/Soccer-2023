5da624870cb142c899d7ffdf6ff6b477e8a135b7
Esteban Martinez <Arepo90@proton.me>
2023-02-20 16:28:22 -0600

    v0.2

--- Soccer/Soccer.ino
+++ Soccer/Soccer.ino
@@ -1,5 +1,5 @@
 #include <EEPROM.h>
-//El 1 es el del arduino y va en sentido horario
+
 //Puente H1 - Atras
 #define EN_1 33
 #define PWM_A1 2
@@ -17,6 +17,19 @@
 #define PWM_A4 3
 #define PWM_B4 5
 
+//Luz 1 - Atras
+#define LUZ_A1 A3
+#define LUZ_B1
+//Luz 2 - Izquierda
+#define LUZ_A2 
+#define LUZ_B2 
+//Luz 3 - Adelante
+#define LUZ_A3 
+#define LUZ_B3 
+//Luz 4 - Derecha
+#define LUZ_A4 
+#define LUZ_B4  
+
 //Ultrasonico 1 - Izquierda
 #define US_E1 34
 #define US_T1 36
@@ -24,16 +37,36 @@
 #define US_E2 29
 #define US_T2 27
 
+#define POWER 80
 
-#define POWER 80;
 
-void setup() {
+
+/*Ultrasonic US_1(US_T1, US_E1);
+Ultrasonic US_2(US_T2, US_E2);*/
+
+/*Light L1(LUZ_A1, LUZ_B1);
+Light L2(LUZ_A2, LUZ_B2);
+Light L3(LUZ_A3, LUZ_B3);
+Light L4(LUZ_A4, LUZ_B4);*/
+
+//Las declaraciones de clases ocupan ir antes del setup
+
+void setup(){
+    Serial.begin(9600);
+
     Motor M1(EN_1, PWM_A1, PWM_B1);
     Motor M2(EN_2, PWM_A2, PWM_B2);
     Motor M3(EN_3, PWM_A3, PWM_B3);
     Motor M4(EN_4, PWM_A4, PWM_B4);
 }
 
-void loop() {
-    movimientos();
+void loop(){
+    digitalWrite(EN_3, HIGH);
+    analogWrite(PWM_A3, 50);
+    analogWrite(PWM_B3, 0);
+    delay(1000);
+    digitalWrite(EN_3, HIGH);
+    analogWrite(PWM_A3, 0);
+    analogWrite(PWM_B3, 50);
+    delay(1000);
 }
--- Soccer/defs.ino
+++ Soccer/defs.ino
@@ -1,4 +1,4 @@
-void save(int n, int target){
+void mem_save(int n, int target){
     for(int i = target*4; i < 4*(target+1); i++){
         EEPROM.write(i, (n >= 255 ? 255 : n));
         if(n >= 255) n -= 255;
@@ -6,7 +6,7 @@ void save(int n, int target){
     }
 }
 
-int leer(int target){
+int mem_read(int target){
     int temp = 0;
     for(int i = target*4; i < 4*(target+1); i++){
         temp += EEPROM.read(i);
@@ -15,19 +15,19 @@ int leer(int target){
 }
 
 class Motor{
-    int EN;
-    int PWM_A;
-    int PWM_B;
-
+    private:
+        int EN;
+        int PWM_A;
+        int PWM_B;
     public:
-        Motor(int en, int pwma, int pwmb);
+        Motor(int EN, int PWM_A, int PWM_B);
         void mover(int pot);
 };
 
-Motor::Motor(int en, int pwma, int pwmb){
-    this->EN = en;
-    this->PWM_A = pwma;
-    this->PWM_B = pwmb;
+Motor::Motor(int EN, int PWM_A, int PWM_B){
+    this->EN = EN;
+    this->PWM_A = PWM_A;
+    this->PWM_B = PWM_B;
     pinMode(EN, OUTPUT);
     pinMode(PWM_A, OUTPUT);
     pinMode(PWM_B, OUTPUT);
@@ -39,21 +39,80 @@ void Motor::mover(int pot){
     analogWrite(PWM_B, (pot < 0 ? pot : 0));
 }
 
-class Luz(){
-    int PIN;
-
+class Light{
+    private:
+        int PIN_A;
+        int PIN_B;
+        int LIM_A;
+        int LIM_B;
     public:
-        Luz(int pin);
-        int leer();
+        Light(int PIN_A, int PIN_B);
+        void calibrate(int LIM_A, int LIM_B);
+        int read(int op);
+        int line();
+};
+
+Light::Light(int PIN_A, int PIN_B){
+    this->PIN_A = PIN_A;
+    this->PIN_B = PIN_B;
+    pinMode(PIN_A, INPUT);
+    pinMode(PIN_B, INPUT);
 }
 
-Luz::Luz(int pin){
-    this->PIN = pin;
-    pinMode(PIN, INPUT);
+void Light::calibrate(int LIM_A, int LIM_B){
+    this->LIM_A = LIM_A;
+    this->LIM_B = LIM_B;
 }
 
-int Luz::leer(){
-    return analogRead(PIN);
+int Light::read(int op){
+    return analogRead(op == 0 ? PIN_A : PIN_B);
+}
+
+int Light::line(){
+    bool temp1 = false, temp2 = false;
+    if(analogRead(PIN_A) <= LIM_A) temp1 = true;
+    if(analogRead(PIN_B) <= LIM_B) temp2 = true;
+    if(temp1 && temp2) return 3;
+    else if(temp2) return 2;
+    else if(temp1) return 1;
+    else return 0;
+}
+
+/*class Ultrasonic{
+    int TRIG;
+    int ECHO;
+    int TIMEOUT;
+    unsigned long prevMicros;
+    public:
+        Ultrasonic(int TRIG, int ECHO, int TIMEOUT = 20000UL);
+        int read();
+        void setTimeout(unsigned long TIMEOUT);
+};
+
+Ultrasonic::Ultrasonic(int TRIG, int ECHO, unsigned long TIMEOUT) {
+    this->TRIG = TRIG;
+    this->ECHO = ECHO;
+    this->TIMEOUT = TIMEOUT;
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+}
+
+int Ultrasonic::read(){
+    digitalWrite(TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+    delay(30);
+    prevMicros = micros();
+    while(!digitalRead(ECHO) && (micros() - prevMicros) <= TIMEOUT);
+    prevMicros = micros();
+    while(digitalRead(ECHO)  && (micros() - prevMicros) <= TIMEOUT);
+    return (micros() - prevMicros)/2;
+}
+
+void setTimeout(int TIMEOUT){
+    this->TIMEOUT = TIMEOUT;
 }
 
 class Comp(){
@@ -70,4 +129,4 @@ Comp::Comp(){
 int Comp::leer(){
     int angulo = 0;
     return angulo;
-}
+}*/
